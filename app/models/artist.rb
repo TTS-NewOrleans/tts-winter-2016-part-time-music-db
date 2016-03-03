9499d511573e6453bf70122612e5095f3c95dad2
Ryan Hageman <improv42@gmail.com>
2016-03-02 19:43:55 -0600

    Add data validations to models

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,6 +1,14 @@
 class Artist < ActiveRecord::Base
   belongs_to :genre
   has_many :songs
+
+  validates :name, presence: true,
+                   length: {minimum: 2},
+                   uniqueness: true
+
+  validates :genre_id, presence: true
+  # This line says the associated genre has to actually exist
+  validates :genre, presence: true
 end
 
 # == Schema Information
